58985543b8e389d0955b4ec5208abe6dc4521017
sheelamramya <sairamyasri999@gmail.com>
2024-11-23 02:02:11 +0530

    second commit

---
+++ src/proj.java
@@ -0,0 +1,11 @@
+public class proj {
+
+    public static void main(String args[]){
+
+        System.out.println("hello welcome");
+        System.out.println("this is the project version1");
+
+    }
+}
+
+//done with version 1
