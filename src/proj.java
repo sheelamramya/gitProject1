96b0106afdaa15040bdce1b2c64a5639b7cee68b
sheelamramya <sairamyasri999@gmail.com>
2024-11-23 02:22:38 +0530

    fourth commit

--- src/proj.java
+++ src/proj.java
@@ -3,12 +3,11 @@ public class proj {
     public static void main(String args[]){
 
         System.out.println("hello welcome");
-        System.out.println("this is the project version2");
+        System.out.println("this is the project version3");
         System.out.println("hello welcome");
-        System.out.println("hello welcome");
-        System.out.println("hello welcome");
+
 
     }
 }
 
-//done with version 2
+//done with version 3
